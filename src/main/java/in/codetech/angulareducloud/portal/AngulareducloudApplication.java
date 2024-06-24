739049061f44196b3f4078622f841e8dc26c5c0d
chandani <suryawanshichandani2001@gmail.com>
2024-06-24 11:42:27 +0530

    changes commited

---
+++ src/main/java/in/codetech/angulareducloud/portal/AngulareducloudApplication.java
@@ -0,0 +1,13 @@
+package in.codetech.angulareducloud.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngulareducloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngulareducloudApplication.class, args);
+	}
+
+}
